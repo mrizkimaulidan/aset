e7d290bd2f53e0f050c8e37133ff24de64fb60ca
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2020-12-08 22:49:01 +0800

    finishing all migrations schema

---
+++ database/migrations/2020_12_08_144330_create_commodities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommoditiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commodities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('commodity_category_id')->constrained();
+            $table->foreignId('commodity_location_id')->constrained();
+            $table->string('unique_commodity_number');
+            $table->string('name');
+            $table->bigInteger('amount');
+            $table->date('register_date');
+            $table->tinyInteger('condition');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commodities');
+    }
+}
