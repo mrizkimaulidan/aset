1d6f3726e92227c58d188dcb981d5a94a01c01c6
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2020-12-08 22:38:11 +0800

    Update users and role migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')->constrained();
+            $table->string('unique_user_number');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('gender');
+            $table->string('phone_number');
+            $table->string('photo');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
