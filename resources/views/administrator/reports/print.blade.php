4850fb7c119939e7c6e0881dd273d7f1e1ab8bb0
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2020-12-13 12:17:20 +0800

    print view starter

--- resources/views/administrator/reports/print.blade.php
+++ resources/views/administrator/reports/print.blade.php
@@ -1,1 +1,80 @@
-{{ dd($commodities) }}
+<html>
+
+<head>
+    <title>PRINT</title>
+</head>
+
+<body>
+    <div align="center">
+        <img src="#" width="10%">
+
+        <h3>Laporan Data Aset</h3>
+        <h3>SMK SAHIDA LEMAHABANG</h3>
+        <hr>
+        <br>
+    </div>
+    <table width="90%" border="0" align="center">
+        <tr>
+            <td>
+                <h4>&nbsp;Hasil Laporan Data Aset Tahun</h4>
+            </td>
+        </tr>
+    </table>
+
+    <table width="90%" border="1" align="center">
+        <tbody>
+            <tr>
+                <td align="center">Id_Aset</td>
+                <td align="center">Nama Aset</td>
+                <td align="center">Jenis Aset</td>
+                <td align="center">Ruangan</td>
+                <td align="center">Jumlah</td>
+                <td align="center">Tanggal Register</td>
+                <td align="center">Kondisi</td>
+                <td align="center">Tanggal Update</td>
+                <td align="center">User</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+        </tbody>
+    </table>
+    <br>
+    <br>
+    <table width="90%" border="0" align="center">
+        <tbody>
+            <tr>
+                <td width="20%">Mengetahui</td>
+                <td width="60%">&nbsp;</td>
+                <td width="20%">Cirebon, ............. 2020</td>
+            </tr>
+            <tr>
+                <td>Kepala Sekolah</td>
+                <td>&nbsp;</td>
+                <td>Staff TU</td>
+            </tr>
+            <tr>
+                <td>............................</td>
+                <td>&nbsp;</td>
+                <td>............................</td>
+            </tr>
+            <tr>
+                <td>NIP.</td>
+                <td>&nbsp;</td>
+                <td>NIP.</td>
+            </tr>
+        </tbody>
+    </table>
+
+
+</body>
+
+</html>
