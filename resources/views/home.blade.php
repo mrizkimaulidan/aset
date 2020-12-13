bc90be5ed49b8a5bc855fbefd29b8c0465b6f871
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2020-12-14 00:37:27 +0800

    clicked fas icon on dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,11 @@
 <div class="row">
     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
-            <div class="card-icon bg-primary">
-                <i class="far fa-user"></i>
-            </div>
+            <a href="{{ route('admin.pengguna.index') }}">
+                <div class="card-icon bg-primary pr-1">
+                    <i class="far fa-user"></i>
+                </div>
+            </a>
             <div class="card-wrap">
                 <div class="card-header">
                     <h4>Jumlah Pengguna</h4>
@@ -19,9 +21,11 @@
     </div>
     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
-            <div class="card-icon bg-danger">
-                <i class="fas fa-th-large"></i>
-            </div>
+            <a href="{{ route('admin.jenis-aset.index') }}">
+                <div class="card-icon bg-danger pr-1">
+                    <i class="fas fa-th-large"></i>
+                </div>
+            </a>
             <div class="card-wrap">
                 <div class="card-header">
                     <h4>Jenis Aset</h4>
@@ -34,9 +38,11 @@
     </div>
     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
-            <div class="card-icon bg-warning">
-                <i class="fas fa-map-marker-alt"></i>
-            </div>
+            <a href="{{ route('admin.ruangan.index') }}">
+                <div class="card-icon bg-warning pr-1">
+                    <i class="fas fa-map-marker-alt"></i>
+                </div>
+            </a>
             <div class="card-wrap">
                 <div class="card-header">
                     <h4>Jumlah Ruangan</h4>
@@ -49,9 +55,11 @@
     </div>
     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
-            <div class="card-icon bg-success">
-                <i class="fas fa-boxes"></i>
-            </div>
+            <a href="{{ route('admin.aset.index') }}">
+                <div class="card-icon bg-success pr-1">
+                    <i class="fas fa-boxes"></i>
+                </div>
+            </a>
             <div class="card-wrap">
                 <div class="card-header">
                     <h4>Jumlah Aset</h4>
