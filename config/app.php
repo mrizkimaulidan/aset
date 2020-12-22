39d12dbce01aa0ff51db7dbdee2d04e183346466
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2020-12-22 19:22:53 +0800

    intervention image setting change

--- config/app.php
+++ config/app.php
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Image' => Intervention\Image\Facades\Image::class,
+        'Image' => Intervention\Image\ImageManagerStatic::class,
     ],
 
 ];
