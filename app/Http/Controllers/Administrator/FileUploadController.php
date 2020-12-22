1f967c79a87fb99dbd64d5f2f4cb2827ca147b1f
mrizkimaulidan <mrizkimaulidanx@gmail.com>
2020-12-22 19:28:01 +0800

    resize image

--- app/Http/Controllers/Administrator/FileUploadController.php
+++ app/Http/Controllers/Administrator/FileUploadController.php
@@ -13,22 +13,16 @@ class FileUploadController extends Controller
 
     public function uploadProfilePicture(Request $request)
     {
-        $image_file = $request->photo;
-        $image_name = Str::uuid() . '.' . $image_file->extension();
+        $image_name = Str::uuid() . '.' . $request->photo->extension();
+
+        $image = Image::make($request->photo);
 
         $request->photo->move(public_path($this->path), $image_name);
-
-        $this->resizeImage($image_file, $image_name);
-
-        return $this->path . $image_name;
-    }
-
-    public function resizeImage($image_file, $image_name)
-    {
-        $image = Image::make($image_file);
 
         $image->resize(150, 150, function ($constraint) {
             $constraint->aspectRatio();
         })->save($this->path . $image_name);
+
+        return $this->path . $image_name;
     }
 }
